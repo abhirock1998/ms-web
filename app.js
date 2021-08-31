cd1fe2f240f65c63ebc3076bd73c75d2d0cd6569
unknown <yaduvanshiabhi1998@gmail.com>
2021-08-31 14:45:52 +0530

    add .snv file for credentials

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const app = require("express")();
 const cors = require("cors");
 const { json } = require("express");
@@ -5,6 +6,7 @@ const nodemailer = require("nodemailer");
 const PORT = process.env.PORT || 9090;
 app.use(cors());
 app.use(json());
+console.log(process.env);
 async function handleNodeMailer(data) {
   var { from_name, email, company_name, company_size, mobile } = data;
   const htmlFormat = `<html> <body>  <h2>New Message</h2><p><b>Name </b>: ${from_name} </p>
@@ -12,32 +14,42 @@ async function handleNodeMailer(data) {
     <p><b>Email </b>:${email}</p>
     <p><b>Company Name </b>: ${company_name}</p>
     <p><b>Compant Size </b>: ${company_size}</p>
-    
-    
-    </body>    </html>`;
+   </body>    </html>`;
+
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
+    host: process.env.HOST_ADDRESS,
+    port: process.env.PORT_NAUMBER,
     secure: true,
+    tls: { rejectUnauthorized: false },
     auth: {
-      user: "USERNAME",
-      pass: "PASSWORD",
+      user: process.env.USER,
+      pass: process.env.PASS,
     },
   });
+
   return await transporter
     .sendMail({
-      from: "123@gmail.com",
-      to: "yaduvanshiabhi1998@gmail.com",
+      from: email,
+      to: "contactus@megasoftsol.com",
       subject: `You get a new message from ${from_name}`,
       html: htmlFormat,
     })
-    .then((res) => res.response);
+    .then((res) => res.response)
+    .catch((e) => e);
 }
 
 app.post("/send", async (req, res) => {
   var result = await handleNodeMailer(req.body);
-  if (result.search("OK") == 10 && result.search("OK") != -1) {
-    res.json({ status: 200, msg: "Sent Seuccesfully" });
+  if (result != undefined) {
+    if (
+      result.search("OK") == 4 &&
+      result.search("OK") != -1 &&
+      typeof result == "string"
+    ) {
+      res.json({ status: 200, msg: "Sent Seuccesfully" });
+    } else {
+      res.json({ status: 404, msg: "Something went wrong !!" });
+    }
   } else {
     res.json({ status: 404, msg: "Something went wrong !!" });
   }
